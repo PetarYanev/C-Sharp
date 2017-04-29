b1bb87f85295e4fca24c127fd22bcba9195dd457
PetarYanev <yanev.petar@gmail.com>
2017-04-29 12:22:02 +0300

    Add files via upload

---
+++ CSharp High Quality Code Part 2/4. Development Tools/T4 template/T4 template/MyTextTemplate.cs
@@ -0,0 +1,19 @@
+﻿
+namespace MoviesStore
+{
+	using System;
+
+	public class T4Example
+	{
+		    public int Movies1 { get; set; }
+		    public int Movies2 { get; set; }
+		    public int Movies3 { get; set; }
+		    public int Movies4 { get; set; }
+		    public int Movies5 { get; set; }
+		    public int Movies6 { get; set; }
+		    public int Movies7 { get; set; }
+		    public int Movies8 { get; set; }
+		    public int Movies9 { get; set; }
+		    public int Movies10 { get; set; }
+		}
+}
---
+++ CSharp High Quality Code Part 2/4. Development Tools/T4 template/T4 template/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace T4_template
+{
+    public class Program
+    {
+        public static void Main()
+        {
+        }
+    }
+}
